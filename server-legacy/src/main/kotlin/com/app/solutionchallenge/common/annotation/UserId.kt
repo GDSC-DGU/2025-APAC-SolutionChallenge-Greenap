d023069bcb45f7d94d4e59c0fc4ddde2a8393cd5
5hseok <ohhs1010@gmail.com>
2025-04-18 17:00:29 +0900

    :tada: begin(server): init server

--- server-legacy/src/main/kotlin/com/app/solutionchallenge/common/annotation/UserId.kt
+++
@@ -1,7 +0,0 @@
-package com.app.solutionchallenge.common.annotation
-
-@MustBeDocumented
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class UserId
-
--- server-legacy/src/main/kotlin/com/app/solutionchallenge/user/application/service/UserService.kt
+++
@@ -1,9 +0,0 @@
-package com.app.solutionchallenge.user.application.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class UserService {
-    fun updateRefreshToken(userId: Long, refreshToken: String?) {
-    }
-}
