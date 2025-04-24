6887e372e5eeb81731ac509ed8c4e34154b976ad
5hseok <ohhs1010@gmail.com>
2025-04-25 04:50:11 +0900

    :bug: fix(user_challenge): ChallengeParticipantRequestDto 매핑 컬럼명 변경

--- server/src/main/kotlin/com/app/server/user_challenge/ui/dto/ChallengeParticipantRequestDto.kt
+++ server/src/main/kotlin/com/app/server/user_challenge/ui/dto/ChallengeParticipantRequestDto.kt
@@ -2,9 +2,12 @@ package com.app.server.user_challenge.ui.dto
 
 import com.app.server.challenge.application.usecase.dto.request.ChallengeParticipantDto
 import com.app.server.user_challenge.domain.enums.EUserChallengeStatus
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ChallengeParticipantRequestDto(
+    @JsonProperty("challenge_id")
     val challengeId: Long,
+    @JsonProperty("participants_date")
     val participantsDate: Int,
 )
 {
