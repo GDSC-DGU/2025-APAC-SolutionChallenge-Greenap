01ef68b0aa67d3ae9bcc44467eb9fbc2ec286aac
5hseok <ohhs1010@gmail.com>
2025-05-13 21:55:31 +0900

    ✨ refactor: comment out MultipartConfig class and its related imports

--- server/src/main/kotlin/com/app/server/common/config/MultipartConfig.kt
+++ server/src/main/kotlin/com/app/server/common/config/MultipartConfig.kt
@@ -1,21 +1,21 @@
-package com.app.server.common.config
-
-import jakarta.servlet.MultipartConfigElement
-import org.springframework.boot.web.servlet.MultipartConfigFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.util.unit.DataSize
-
-@Configuration
-class MultipartConfig {
-
-    @Bean
-    fun multipartConfigElement(): MultipartConfigElement {
-        val factory = MultipartConfigFactory()
-        // 최대 파일 크기
-        factory.setMaxFileSize(DataSize.ofMegabytes(50))
-        // 최대 요청 크기
-        factory.setMaxRequestSize(DataSize.ofMegabytes(50))
-        return factory.createMultipartConfig()
-    }
-}
+//package com.app.server.common.config
+//
+//import jakarta.servlet.MultipartConfigElement
+//import org.springframework.boot.web.servlet.MultipartConfigFactory
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.util.unit.DataSize
+//
+//@Configuration
+//class MultipartConfig {
+//
+//    @Bean
+//    fun multipartConfigElement(): MultipartConfigElement {
+//        val factory = MultipartConfigFactory()
+//        // 최대 파일 크기
+//        factory.setMaxFileSize(DataSize.ofMegabytes(50))
+//        // 최대 요청 크기
+//        factory.setMaxRequestSize(DataSize.ofMegabytes(50))
+//        return factory.createMultipartConfig()
+//    }
+//}
