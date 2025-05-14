78901e499e00c123c715a0bfac1f7f5184befb58
5hseok <ohhs1010@gmail.com>
2025-05-14 17:57:06 +0900

    ✨ feat(feed): enhance API documentation for feed operations with detailed descriptions

--- server/src/main/kotlin/com/app/server/notification/ui/controller/NotificationController.kt
+++ server/src/main/kotlin/com/app/server/notification/ui/controller/NotificationController.kt
@@ -5,6 +5,7 @@ import com.app.server.common.response.ApiResponse
 import com.app.server.notification.ui.dto.request.GetEncourageMessageRequestDto
 import com.app.server.notification.ui.dto.response.GetEncourageMessageResponseDto
 import com.app.server.notification.ui.usecase.GetEncourageMessageUseCase
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -17,6 +18,10 @@ class NotificationController(
 ) {
 
     @GetMapping("/encourage")
+    @Operation(
+        summary = "챌린지 참여 독려 메시지 조회",
+        description = "챌린지 참여 독려 메시지를 조회합니다."
+    )
     fun getEncourageMessage(
         @UserId userId: Long,
     ): ApiResponse<GetEncourageMessageResponseDto> {
