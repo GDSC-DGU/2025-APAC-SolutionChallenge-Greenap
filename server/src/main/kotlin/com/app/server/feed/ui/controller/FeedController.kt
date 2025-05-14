eb3f7847df6247cff5a69e3f07b08c2c3cc67060
5hseok <ohhs1010@gmail.com>
2025-05-14 17:57:06 +0900

    ✨ feat(feed): enhance API documentation for feed operations with detailed descriptions

--- server/src/main/kotlin/com/app/server/feed/ui/controller/FeedController.kt
+++ server/src/main/kotlin/com/app/server/feed/ui/controller/FeedController.kt
@@ -28,11 +28,11 @@ class FeedController(
     @GetMapping
     @Operation(
         summary = "피드 조회",
-        description = "피드를 조회합니다. 카테고리 ID, 스코프, 유저 챌린지 ID, 페이지, 사이즈를 입력하세요." +
-                "// page, size는 요청하기 나름 \n" +
-                "모든 사용자가 작성한 전체 피드를 조회하고 싶다 -> /api/v1/feeds\n" +
-                "특정 카테고리의 모든 피드를 조회하고 싶다 -> /api/v1/feeds?category_id=1\n" +
-                "특정 사용자가 참여했던 챌린지들 중 특정 카테고리 내 챌린지들에서의 모든 피드를 조회하고 싶다. -> /api/v1/feeds?category_id=1&scope=user\n" +
+        description = "피드를 조회합니다. <br> 카테고리 ID, 스코프, 유저 챌린지 ID, 페이지, 사이즈를 입력하세요. <br>" +
+                "<br>page, size는 기본값 1, 7로 설정되어 있고 새롭게 요청할 수도 있습니다.<br><br>" +
+                "모든 사용자가 작성한 전체 피드를 조회하고 싶다 -> /api/v1/feeds <br>" +
+                "특정 카테고리의 모든 피드를 조회하고 싶다 -> /api/v1/feeds?category_id=1 <br>" +
+                "특정 사용자가 참여했던 챌린지들 중 특정 카테고리 내 챌린지들에서의 모든 피드를 조회하고 싶다. -> /api/v1/feeds?category_id=1&scope=user <br>" +
                 "특정 사용자가 참여 중인 혹은 참여했던 특정 챌린지에서 작성한 모든 피드들을 조회하고 싶다. -> /api/v1/feeds?scope=user&user_challenge_id=1"
     )
     fun readFeed(
@@ -58,6 +58,10 @@ class FeedController(
     }
 
     @PostMapping
+    @Operation(
+        summary = "피드 작성",
+        description = "피드를 작성합니다."
+    )
     fun createFeed(
         @UserId userId: Long,
         @RequestBody createFeedRequestDto: CreateFeedRequestDto
@@ -69,6 +73,10 @@ class FeedController(
     }
 
     @PutMapping("/{feedId}")
+    @Operation(
+        summary = "피드 수정",
+        description = "피드를 수정합니다."
+    )
     fun updateFeed(
         @PathVariable feedId: Long,
         @RequestParam("content") newContent: String
@@ -78,6 +86,10 @@ class FeedController(
     }
 
     @DeleteMapping("/{feedId}")
+    @Operation(
+        summary = "피드 삭제",
+        description = "피드를 삭제합니다."
+    )
     fun deleteFeed(
         @PathVariable feedId: Long
     ) : ApiResponse<ResultCode> {
